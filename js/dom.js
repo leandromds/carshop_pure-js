acc47a718b5e22babac43073dfc7705c19b45db7
Leandro Manoel da Silva <mds2.design@gmail.com>
2018-03-09 14:52:53 -0300

    Update dom.js
    The validate object function was refactored.

--- js/dom.js
+++ js/dom.js
@@ -1,85 +1,91 @@
 ( function( win, doc ) {
-    'use strict';
+  'use strict';
 
-    function DOM( elements ) {
-        if( !( this instanceof DOM ) )
-            return new DOM( elements );
-        this.element = doc.querySelectorAll( elements );
-    }
+  function DOM( elements ) {
+    if( !( this instanceof DOM ) )
+        return new DOM( elements );
+    this.element = doc.querySelectorAll( elements );
+  }
 
-    DOM.isArray = function isArray( param ) {
-        return Object.prototype.toString.call( param ) === '[object Array]';
-    };
+  DOM.isObject = function isObject( param ) {
+    return Object.prototype.toString.call( param );
+  }
 
-    DOM.isFunction = function isFunction( param ) {
-        return Object.prototype.toString.call( param ) === '[object Function]';
-    };
+  DOM.validateObject = (function() {
+    var defaultObjects = [ 
+      'isArray', 
+      'isFunction', 
+      'isObject', 
+      'isNumber', 
+      'isString', 
+      'isBoolean', 
+      'isNull' ];
+    var defaultValidateMethods = {};
+    defaultObjects.forEach( function( obj ) {
+      return defaultValidateMethods[ obj ] = function() {
+        return function( param ) {
+          if( arguments[1] )
+            return this.isObject( param ) === '[object Null]' || this.isObject( param ) === '[object Undefined]';           
+          return this.isObject( param ) === objName;
+        }
+      };
+    } );
+    return defaultValidateMethods;
+  })();
 
-    DOM.isObject = function isObject( param ) {
-        return Object.prototype.toString.call( param ) === '[object Object]';
-    };
+  DOM.isArray = DOM.validateObject.isArray( '[object Array]' );
+  DOM.isFunction = DOM.validateObject.isFunction( '[object Function]' );
+  DOM.isObject = DOM.validateObject.isObject( '[object Object]' );
+  DOM.isNumber = DOM.validateObject.isNumber( '[object Number]' );
+  DOM.isString = DOM.validateObject.isString( '[object String]' );
+  DOM.isBoolean = DOM.validateObject.isBoolean( '[object Boolean]' );
+  DOM.isNull = DOM.validateObject.isNull( '[object Null]', '[object Undefined]' );
 
-    DOM.isNumber = function isNumber( param ) {
-        return Object.prototype.toString.call( param ) === '[object Number]';
-    };
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply( this.element, arguments);
+  };
 
-    DOM.isString = function isString( param ) {
-        return Object.prototype.toString.call( param ) === '[object String]';
-    };
+  DOM.prototype.map = function map() {
+      return Array.prototype.map.apply( this.element, arguments );
+  };
 
-    DOM.isBoolean = function isBoolean( param ) {
-        return Object.prototype.toString.call( param ) === '[object Boolean]';
-    };
+  DOM.prototype.filter = function filter() {
+      return Array.prototype.filter.apply( this.element, arguments );
+  };
 
-    DOM.isNull = function isNull( param ) {
-        return Object.prototype.toString.call( param ) === '[object Null]' || this.is( param ) === '[object Undefined]' 
-    };
+  DOM.prototype.reduce = function reduce() {
+      return Array.prototype.reduce.apply( this.element, arguments );
+  };
 
-    DOM.prototype.forEach = function forEach() {
-        return Array.prototype.forEach.apply( this.element, arguments);
-    };
+  DOM.prototype.reduceRight = function reduceRight() {
+      return Array.prototype.reduceRight.apply( this.element, arguments );
+  };
 
-    DOM.prototype.map = function map() {
-        return Array.prototype.map.apply( this.element, arguments );
-    };
+  DOM.prototype.every = function every() {
+      return Array.prototype.every.apply( this.element, arguments );
+  };
 
-    DOM.prototype.filter = function filter() {
-        return Array.prototype.filter.apply( this.element, arguments );
-    };
+  DOM.prototype.some = function some() {
+      return Array.prototype.some.apply( this.element, arguments );
+  };
 
-    DOM.prototype.reduce = function reduce() {
-        return Array.prototype.reduce.apply( this.element, arguments );
-    };
+  DOM.prototype.on = function on( eventType, callback ) {
+      this.forEach( function( $element ){ 
+          $element.addEventListener( eventType, callback, false);
+      });
+  };
 
-    DOM.prototype.reduceRight = function reduceRight() {
-        return Array.prototype.reduceRight.apply( this.element, arguments );
-    };
+  DOM.prototype.off = function off( eventType, callback ) {
+      this.forEach( function( element ) {
+          $element.removeEventListener( eventType, callback, false );
+      });
+  };
 
-    DOM.prototype.every = function every() {
-        return Array.prototype.every.apply( this.element, arguments );
-    };
+  DOM.prototype.get = function get( index ) {
+      if( !index )
+          return this.element[ 0 ];
+      return this.element[ index ];
+  };
 
-    DOM.prototype.some = function some() {
-        return Array.prototype.some.apply( this.element, arguments );
-    };
-
-    DOM.prototype.on = function on( eventType, callback ) {
-        this.forEach( function( $element ){ 
-            $element.addEventListener( eventType, callback, false);
-        });
-    };
-
-    DOM.prototype.off = function off( eventType, callback ) {
-        this.forEach( function( element ) {
-            $element.removeEventListener( eventType, callback, false );
-        });
-    };
-
-    DOM.prototype.get = function get( index ) {
-        if( !index )
-            return this.element[ 0 ];
-        return this.element[ index ];
-    };
-
-    win.DOM = DOM;
+  win.DOM = DOM;
 })( window, document );
